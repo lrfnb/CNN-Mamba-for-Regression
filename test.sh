3e9614c1b74bf95d2e75f3edc23b8d26b7f99a0e
lrfnb <108459926+lrfnb@users.noreply.github.com>
2025-12-17 11:26:26 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,2 @@
 # On CelebA Test set
-python test.py --gpus 1 --model CMCNet --name CMCNET \
-    --load_size 128 --dataset_name single --dataroot /Test_Celeba \
-    --pretrain_model_path ./pretrain_models/... \
-    --save_as_dir results_celeba/cmcnet
+python test_regression.py
