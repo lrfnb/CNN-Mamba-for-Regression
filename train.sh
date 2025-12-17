de36d8382fdbea499275eb5a45b9f27f179e473d
lrfnb <108459926+lrfnb@users.noreply.github.com>
2025-12-17 11:27:09 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,3 +1,1 @@
-# Train Code
-# Train Code
 python train.py --gpus 1 --name ResolutionRegression --model resolutionregressionnet --Gnorm "bn" --lr 0.0001 --beta1 0.9 --load_size 128  --dataroot /data2/lrf/data/spatial-resolution_0.1m/train_mix --dataset_name remotesensingresolution --batch_size 12 --total_epochs 100 --visual_freq 20 --print_freq 50 --save_latest_freq 1000
